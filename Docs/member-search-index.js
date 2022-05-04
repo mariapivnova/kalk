a42ee27dd4b872235fbf01399002cd878bcab493
maria <mariapivnova@yandex.ru>
2022-05-04 11:09:51 +0300

    commit

---
+++ Docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.company","c":"Main","l":"calculate(String[], double)","url":"calculate(java.lang.String[],double)"},{"p":"com.company","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"com.company","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"}]
